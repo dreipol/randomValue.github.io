3eee8eba260c30a613211cf5fb65f280d865df17
Thibault Badoux <thibault.badoux@gmail.com>
2021-01-26 10:22:23 +0100

    remove MUI Palette darkmode for this example

--- theming/mui-theme.js
+++ theming/mui-theme.js
@@ -1,15 +1,12 @@
-import { createMuiTheme } from "@material-ui/core";
-import CustomTheme from "./custom-theme";
-import darkColors from "./colors-dark.json";
-import lightColors from "./colors-light.json";
-
-export const Theme = (isDarkmode) => {
+import { createMuiTheme } from '@material-ui/core'
+import CustomTheme from './custom-theme'
+import darkColors from './colors-dark.json'
+import lightColors from './colors-light.json'
+export const Theme = () => {
   return createMuiTheme({
-    ...CustomTheme(
-      isDarkmode
-        ? { ...lightColors.colors, ...darkColors.colors }
-        : lightColors.colors,
-      isDarkmode
-    )
-  });
-};
+    ...CustomTheme(lightColors.colors),
+    palette: {
+      ...CustomTheme(lightColors.colors).palette,
+    },
+  })
+}
