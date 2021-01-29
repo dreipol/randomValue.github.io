7d67c3f6ff27c5a04bfd9dfa782e6452aab5f5c6
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2021-01-29 11:53:21 +0100

    [gh-pages] Log out router

--- pages/_app.js
+++ pages/_app.js
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps, router }) {
 
   const { asPath } = router
   
-  console.log(asPath)
+  console.log(router)
   
   useEffect(() => {
     importColorVars(asPath)
