12e127cfb305f1c56a4b2d6f6df52803eedce107
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2021-01-29 10:02:46 +0100

    [gh-pages] Add gh actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 var jsonImporter = require('node-sass-json-importer')
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   sassOptions: {
     importer: jsonImporter(),
   },
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
 }
