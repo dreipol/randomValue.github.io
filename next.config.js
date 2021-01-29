ba23a31697058e6dd5c664b08b7e79c94ae5a2d5
randomValue <50024618+randomValue@users.noreply.github.com>
2021-01-29 10:54:41 +0000

    Deploying to gh-pages from  @ 7d67c3f6ff27c5a04bfd9dfa782e6452aab5f5c6 🚀

--- next.config.js
+++
@@ -1,9 +0,0 @@
-var jsonImporter = require('node-sass-json-importer')
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  sassOptions: {
-    importer: jsonImporter(),
-  },
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/dreipol/randomvalue.github.io/gh-pages/' : '',
-}
--- src/theming/mui-theme-dark-mode.js
+++
@@ -1,6 +0,0 @@
-import { createMuiTheme } from '@material-ui/core'
-import CustomTheme from './custom-theme-dark-mode'
-
-export const DarkMode = () => {
-  return createMuiTheme(CustomTheme)
-}
